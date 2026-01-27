6fc73e0e8141b37372c525b3c21066e164dbb3de
KTHOMASre96 <KTHOMAS94846@uat.edu>
2026-01-26 19:14:42 -0600

    Add files via upload
    a html page and JavaScript Functions to the html page

---
+++ code.js
@@ -0,0 +1,8 @@
+// function updateText is a function to change the heading from welcome to my page to Play Me and Play On!! 
+function updateText(){
+    document.getElementById("heading").innerHTML ="Play Me and Play On!!"; 
+}
+// function open is a function that opens a link to a math gaming website
+function open(){
+    document.getElementById(website).innerHTML ="https://www.abcya.com";
+}
